cafa3b13bc9e2e06406946bc198c04c80a55c31b
Aspect-6 <neelpatel246624@gmail.com>
2024-02-17 13:14:15 -0500

    shorten code

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -5,7 +5,6 @@
         setTimeout(() => {
             const totalPoints =
                 document.getElementsByClassName("total_points")[0]
-            console.log("Total points: ", totalPoints.innerText)
             if (totalPoints.innerText >= 200) {
                 document.getElementsByClassName("quitgame")[0].click()
                 return
@@ -24,28 +23,26 @@
         }
     })
 
-    let transcript = ""
-
     // Get transcript
-    for (let i = 0; i < document.getElementsByTagName("script").length; i++) {
-        document
-            .getElementsByTagName("script")
-            [i].innerText.includes("var captions") && // If the scipt contains the transcript, continue
-            JSON.parse(
-                document
-                    .getElementsByTagName("script")
-                    [i].innerText.split("\t")[3]
-                    .split("var captions = $.extend(")
-                    .join("")
-                    .split(", PlayerCommon.Mixins.Captions),\n")
-                    .join("")
-                    .replace(/(\r\n|\n|\r)/gm, "")
-            ).forEach((item) =>
-                item.transcript_words.forEach(
-                    (word_obj) => (transcript += word_obj.word + " ")
+    let transcript = Array.from(document.getElementsByTagName("script"))
+        .map(
+            (script) =>
+                script.innerText.includes("var captions") && // If the script contains the transcript, continue
+                JSON.parse(
+                    script.innerText
+                        .split("\t")[3]
+                        .split("var captions = $.extend(")
+                        .join("")
+                        .split(", PlayerCommon.Mixins.Captions),\n")
+                        .join("")
+                        .replace(/(\r\n|\n|\r)/gm, "")
+                ).map(({ transcript_words }) =>
+                    transcript_words.map(({ word }) => word)
                 )
-            )
-    }
+        )
+        .filter((val) => val)[0]
+        .map((arr) => arr.join(" "))
+        .join(" ")
 
     function answerQuestion() {
         let words = document.getElementsByClassName("question")[0].children
@@ -53,44 +50,38 @@
         let options =
             document.getElementsByClassName("choice_buttons")[0].children
 
-        let underlineIndex
-        let correctAnswerIndex
+        let underlineIndex, correctAnswerIndex
 
         // Get index of the underline in the question div
         for (let i = 0; i < words.length; i++)
             if (words[i].classList.contains("underline")) underlineIndex = i
 
-        // If the underline is not the first word
-        if (underlineIndex !== 0) {
-            let wordsBeforeUnderline = ""
-
-            // Find the words before the underline
-            for (let i = 0; i < underlineIndex; i++)
-                wordsBeforeUnderline += words[i].innerText + " "
-
-            // Find the correct word
-            for (let i = 0; i < options.length; i++)
-                if (
-                    transcript.includes(
-                        wordsBeforeUnderline + options[i].innerText + " "
-                    )
-                )
-                    correctAnswerIndex = i
-        } else {
-            // If the underline is the first word
+        // If the underline is the first word
+        if (underlineIndex === 0) {
             let wordsAfterUnderline = ""
 
             // Find the words after the underline
             for (let i = 1; i < words.length; i++)
-                wordsAfterUnderline += words[i].innerText
+                wordsAfterUnderline += `${words[i].innerText} `
             // Find the correct word
-            for (let i = 0; i < options.length; i++)
-                if (
-                    transcript.includes(
-                        " " + options[i].innerText + wordsAfterUnderline
-                    )
-                )
-                    correctAnswerIndex = i
+            Array.from(options).forEach((option, i) => {
+                transcript.includes(
+                    ` ${option.innerText}${wordsAfterUnderline}`
+                ) && (correctAnswerIndex = i)
+            })
+        } else {
+            let wordsBeforeUnderline = ""
+
+            // Find the words before the underline
+            for (let i = 0; i < underlineIndex; i++)
+                wordsBeforeUnderline += `${words[i].innerText} `
+
+            // Find the correct word
+            Array.from(options).forEach((option, i) => {
+                transcript.includes(
+                    `${wordsBeforeUnderline}${option.innerText} `
+                ) && (correctAnswerIndex = i)
+            })
         }
 
         // Select the correct option
